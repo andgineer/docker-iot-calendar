d0fc226f48a741eb92355a55f2dfb12235bec1f4
Andrey Sorokin <sorokin@tprs.ru>
2017-04-19 17:02:30 +0300

    data format refactoring

--- src/calendar_data.py
+++ src/calendar_data.py
@@ -39,134 +39,122 @@ class CalendarData(object):
         return result
 
 
-def interval_by_time(time, by_time=[]):
-    if len(by_time) == 0:
-        return 0
-    for interval_idx, interval in enumerate(by_time):
-        if 'before' in interval:
-            interval_end_parts = [int(s) for s in interval['before'].split(':')]
-            interval_end = time.replace(hour=interval_end_parts[0], minute=interval_end_parts[1], second=interval_end_parts[2])
-            if time < interval_end:
-                return interval_idx
-    else:
-        return interval_idx
-
-
-def get_values_empty_array(by_time):
-    if len(by_time) == 0:
-        values_array_len = 1
-    else:
-        values_array_len = len(by_time)
-    return np.zeros(values_array_len)
-
-
 def event_duration(event):
     return int((event['end'] - event['start']).seconds / 60)
 
 
-def events_to_weeks_grid(events, summary, weeks=4, by_time=[]):
+def events_to_weeks_grid(events, weeks=4):
     """
+    events: list of events lists
+    returns weeks list:
     [
-        [{'date': week0_day0_date, 'values': values_list}, ...],
-        [{'date': week1_day0_date, 'values': values_list}, ...],
+        [{'date': week0_Monday_date, 'values': values_list}, {'date': week0_Tuesday_date, 'values': values_list}, ...],
+        [{'date': week1_Monday_date, 'values': values_list}, {'date': week1_Tuesday_date, 'values': values_list}, ...],
+        ...
+        [{'date': week<weeks-1>_Monday_date, 'values': values_list}, {'date': week<weeks-1>_Tuesday_date, 'values': values_list}, ...]
     ]
     """
-    if len(events) > 0:
-        tzinfo = events[0]['start'].tzinfo
-    else:
-        tzinfo = None
-    last_date_in_grid = datetime.datetime.now(tzinfo).replace(hour=0, minute=0, second=0, microsecond=0)
-    today_week_day = last_date_in_grid.weekday()
-    first_date_in_grid = last_date_in_grid - datetime.timedelta(days=weeks * 7 - (7 - (today_week_day + 1)) - 1)
+    def get_tzinfo(events):
+        """get tzinfo from any event"""
+        for event_list in events:
+            for event in event_list:
+                return event['start'].tzinfo
+        return None
+    tzinfo = get_tzinfo(events)
+    today = datetime.datetime.now(tzinfo).replace(hour=0, minute=0, second=0, microsecond=0)
+    tomorrow = today + datetime.timedelta(days=1)
+    today_week_day = today.weekday()
+    first_date_in_grid = today - datetime.timedelta(days=weeks * 7 - (7 - (today_week_day + 1)) - 1)
     grid = []
-
     for week in range(weeks):
         week_array = []
         for day in range(7):
             week_array.append({
                 'date': first_date_in_grid + datetime.timedelta(weeks=week, days=day),
-                'values': get_values_empty_array(by_time)
+                'values': np.zeros(len(events))
             })
         grid.append(week_array)
     if len(events) == 0:
         return grid
-    for event in events:
-        time = event['start']
-        if time >= first_date_in_grid and time < last_date_in_grid + datetime.timedelta(days=1):
-            week = int((time - first_date_in_grid).days // 7)
-            day = int((time - first_date_in_grid -datetime.timedelta(weeks=week)).days)
-            value_idx = interval_by_time(time, by_time)
-            grid[week][day]['values'][value_idx] += event_duration(event)
+    for event_list_idx, event_list in enumerate(events):
+        for event in event_list:
+            time = event['start']
+            if first_date_in_grid <= time < tomorrow:
+                week = int((time - first_date_in_grid).days // 7)
+                day = int((time - first_date_in_grid -datetime.timedelta(weeks=week)).days)
+                grid[week][day]['values'][event_list_idx] += event_duration(event)
     return grid
 
 
-def events_to_array(events, summary, by_time=[]):
-    a = []
-    for event in events:
-        date = event['start'].replace(hour=0, minute=0, second=0, microsecond=0)
-        value_idx = interval_by_time(event['start'], by_time)
-        if len(a) == 0 or a[-1]['date'] != date:
-            a.append({'date': date, 'values': get_values_empty_array(by_time)})
-        a[-1]['values'][value_idx] += event_duration(event)
-    return [event['date'] for event in a], [[event['values'][0] for event in a], [event['values'][1] for event in a]]
+def events_to_array(events):
+    DATE_FMT = '%Y%m%d'
+    by_date = {}
+    for event_list_idx, event_list in enumerate(events):
+        for event in event_list:
+            date = event['start'].replace(hour=0, minute=0, second=0, microsecond=0)
+            date_str = date.strftime(DATE_FMT)
+            if date_str not in by_date:
+                by_date[date_str] = np.zeros(len(events))
+            by_date[date_str][event_list_idx] += event_duration(event)
+    x = [datetime.datetime.strptime(date_str, DATE_FMT) for date_str in sorted(by_date.keys())]
+    y = []
+    for event_list_idx in range(len(events)):
+        y.append([by_date[date_str][event_list_idx] for date_str in sorted(by_date.keys())])
+    return x, y
 
 
 def test():
     events = [
-        {'end': datetime.datetime(2017, 4, 6, 20, 17, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 6, 20, 2, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 7, 8, 17, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 7, 8, 2, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 7, 19, 12, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 7, 18, 55, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 8, 11, 11, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 8, 11, 2, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 11, 19, 53, 19, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 11, 19, 33, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 12, 8, 30, 11, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 12, 8, 20, 50, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 12, 20, 39, 3, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 12, 20, 18, 29, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 13, 8, 19, 22, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 13, 8, 13, 24, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 13, 20, 23, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 13, 20, 3, 5, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 14, 8, 22, 30, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 14, 8, 15, 39, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 14, 17, 26, 47, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 14, 16, 55, 56, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 16, 18, 20, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 16, 17, 48, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 17, 8, 27, 43, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 17, 8, 16, 35, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'},
-        {'end': datetime.datetime(2017, 4, 17, 17, 51, 35, tzinfo=tzoffset(None, 10800)),
-         'start': datetime.datetime(2017, 4, 17, 17, 28, 31, tzinfo=tzoffset(None, 10800)),
-         'summary': '#white'}
+        [
+            {'end': datetime.datetime(2017, 4, 7, 8, 17, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 7, 8, 2, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Morning work-out'},
+            {'end': datetime.datetime(2017, 4, 8, 11, 11, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 8, 11, 2, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Morning work-out'},
+            {'end': datetime.datetime(2017, 4, 12, 8, 30, 11, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 12, 8, 20, 50, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Morning work-out'},
+            {'end': datetime.datetime(2017, 4, 13, 8, 19, 22, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 13, 8, 13, 24, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Morning work-out'},
+            {'end': datetime.datetime(2017, 4, 14, 8, 22, 30, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 14, 8, 15, 39, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Morning work-out'},
+            {'end': datetime.datetime(2017, 4, 17, 8, 27, 43, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 17, 8, 16, 35, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Morning work-out'},
+        ],
+        [
+            {'end': datetime.datetime(2017, 4, 6, 20, 17, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 6, 20, 2, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 7, 19, 12, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 7, 18, 55, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 11, 19, 53, 19, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 11, 19, 33, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 12, 20, 39, 3, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 12, 20, 18, 29, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 13, 20, 23, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 13, 20, 3, 5, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 14, 17, 26, 47, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 14, 16, 55, 56, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 16, 18, 20, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 16, 17, 48, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'},
+            {'end': datetime.datetime(2017, 4, 17, 17, 51, 35, tzinfo=tzoffset(None, 10800)),
+             'start': datetime.datetime(2017, 4, 17, 17, 28, 31, tzinfo=tzoffset(None, 10800)),
+             'summary': 'Physiotherapy'}
+        ]
     ]
-    x, y = events_to_array(
-        events,
-        'white',
-        by_time=[{'before': '11:00:00', 'summary': 'morning {summary}'}, {'summary': 'evening {summary}'}]
-    )
+    x, y = events_to_array(events)
     print(x, y)
-    grid = events_to_weeks_grid(
-        events,
-        'white',
-        by_time=[{'before': '11:00:00', 'summary': 'morning {summary}'}, {'summary': 'evening {summary}'}]
-    )
+    grid = events_to_weeks_grid(events)
     print(grid)
 
 
--- src/calendar_image.py
+++ src/calendar_image.py
@@ -1,6 +1,5 @@
 #import svgwrite
 import datetime
-from dateutil.tz import tzoffset
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
@@ -16,7 +15,6 @@ import matplotlib.image as mpimg
 explode = (0.1, 0.05)
 
 weeks = 4
-days = 7
 
 dpi = 100
 picture_width = 8 # * dpi = 800
@@ -43,73 +41,179 @@ plot_height = 1 - pies_height - 0.1
 plot_bottom = 1 - plot_height
 
 
-def draw_plot(axes, x, y, legend_labels):
-    polys = axes.stackplot(x, y)
+class ImageHandler(HandlerBase):
+    def create_artists(self, legend, orig_handle,
+                       xdescent, ydescent, width, height, fontsize,
+                       trans):
+
+        # enlarge the image by these margins
+        sx, sy = self.image_stretch
+
+        # create a bounding box to house the image
+        bb = Bbox.from_bounds(xdescent - sx,
+                              ydescent - sy,
+                              width + sx,
+                              height + sy)
+
+        tbb = TransformedBbox(bb, trans)
+        image = BboxImage(tbb)
+        image.set_data(self.image_data)
+
+        self.update_prop(image, orig_handle, legend)
+
+        return [image]
+
+    def set_image(self, image_path, image_stretch=(0, 0)):
+        if os.path.exists(image_path):
+            self.image_data = read_png(image_path)
+        self.image_stretch = image_stretch
+
+from matplotlib.legend_handler import HandlerPatch
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+class HandlerRectangle(HandlerPatch):
+    def create_artists(self, legend, orig_handle,
+                       xdescent, ydescent, width, height, fontsize, trans):
+        bb = Bbox.from_bounds(xdescent - 0,
+                              ydescent - 20,
+                              width + 0,
+                              height + 8)
+
+        tbb = TransformedBbox(bb, trans)
+        image = BboxImage(tbb)
+        image.set_data(self.image_data)
+
+        self.update_prop(image, orig_handle, legend)
+        image.set_transform(trans)
+        return [image]
+
+    def set_image(self, image_path, image_stretch=(0, 0)):
+        if os.path.exists(image_path):
+            self.image_data = read_png(image_path)
+        self.image_stretch = image_stretch
+
+def draw_plot(axes, x, y, legend_labels, legend='rectangle'):
+    polies = axes.stackplot(x, y)
     axes.patch.set_visible(False)
-    legendProxies = []
-    for poly in polys:
-        legendProxies.append(plt.Rectangle((0, 0), 1, 1, fc=poly.get_facecolor()[0]))
-    plt.legend(legendProxies, legend_labels)
-    #plt.legend([s, s2],
-    #       ['Scatters 1', 'Scatters 2'],
-    #       #handler_map={s: custom_handler, s2: custom_handler},
-    #       labelspacing=2,
-    #       frameon=False)
+    if legend == 'rectangle':
+        plt.legend([plt.Rectangle((0, 0), 1, 1, fc=poly.get_facecolor()[0]) for poly in polies], legend_labels)
+    elif legend == 'icons':
+        legend_proxies = []
+        handler_map = {}
+        for poly in polies:
+            patch = mpatches.Rectangle((0, 0), 0.1, 0.1, facecolor=poly.get_facecolor()[0])
+            legend_proxies.append(patch)
+            image_handler = HandlerRectangle() #ImageHandler()
+            image_handler.set_image(image_path='old-woman.png', image_stretch=(0, 16))
+            handler_map[poly] = image_handler
+        plt.legend(
+            legend_proxies,
+            legend_labels,
+            handleheight=3,
+            handlelength=4,
+            #handles=[custom_handler],
+            #handler_map={mpatches.Rectangle: image_handler},
+            handler_map=handler_map, #{s: custom_handler, s2: custom_handler},
+            labelspacing=1,
+            frameon=True
+        )
+    elif legend == 'inside':
+        #loc = y1.argmax()
+        #ax.text(loc, y1[loc]*0.25, areaLabels[0])
+        pass
 
+def draw_pies(grid, weeks=4, empty_image_file_name=None):
+    def find_max_total(grid):
+        max_total = 0
+        for week in range(len(grid)):
+            for day in range(len(grid[week])):
+                total = sum(grid[week][day]['values'])
+                if total > max_total:
+                    max_total = total
+        return max_total
 
-def draw_pies(grid):
-    max_total = 0
-    for week in range(weeks):
-        for day in range(days):
-            total = sum(fracs[week][day])
-            if total > max_total:
-                max_total = total
+    def draw_day_header(day):
+        axs.append(
+            plt.axes([
+                pie_row_header_width + day * pie_width ,
+                weeks * pie_height,
+                pie_width,
+                pie_col_header_height
+            ],
+                facecolor='white'))
+        plt.text(
+            0.5, 0.5,
+            grid[0][day]['date'].strftime('%A'),
+            horizontalalignment='center',
+            verticalalignment='center',
+            fontsize=12
+        )
+        plt.axis('off')
 
-    img = mpimg.imread('old-woman.png')
+    def draw_week_header(week):
+        axs.append(
+            plt.axes([
+                0,
+                week * pie_height,
+                pie_row_header_width,
+                pie_height
+            ],
+                facecolor='white'))
+        plt.text(
+            0.5, 0.5,
+            grid[week][0]['date'].strftime('%d\n%b'),
+            horizontalalignment='center',
+            verticalalignment='center',
+            fontsize=14
+        )
+        plt.axis('off')
+
+    def draw_pie(week, day, values):
+        axs.append(
+            plt.axes([
+                pie_row_header_width + day * pie_width,
+                week * pie_height,
+                pie_width,
+                pie_height
+            ],
+                facecolor='white'))
+        plt.axis('off')
+        radius = sum(values) / max_total
+        plt.pie(values, shadow=True, explode=explode, radius=radius)
+
+    def draw_empty_pie(week, day):
+        axs.append(
+            plt.axes([
+                pie_row_header_width + day * pie_width + image_padding,
+                week * pie_height + image_padding,
+                pie_width - image_padding * 2,
+                pie_height - image_padding * 2
+            ],
+                facecolor='white'
+            )
+        )
+        plt.axis('off')
+        if grid[week][day]['date'] < tomorrow and empty_image_file_name:
+            plt.imshow(img, aspect=1)
+
+    max_total = find_max_total(grid)
+    if empty_image_file_name:
+        img = mpimg.imread(empty_image_file_name)
     tomorrow = datetime.datetime.now(grid[0][0]['date'].tzinfo).replace(hour=0, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
     axs = []
-    for row in range(weeks + 1):
-        if row == 0:
-            for day in range(days):
-                axs.append(plt.axes([pie_row_header_width + day * pie_width , weeks * pie_height, pie_width, pie_col_header_height], facecolor='white'))
-                plt.text(
-                    0.5, 0.5,
-                    grid[0][day]['date'].strftime('%A'),
-                    horizontalalignment='center',
-                    verticalalignment='center',
-                    fontsize=12
-                )
-                plt.axis('off')
-        else:
-            for col in range(days + 1):
-                if col == 0:
-                    axs.append(plt.axes([0, (row - 1) * pie_height, pie_row_header_width, pie_height], facecolor='white'))
-                    plt.text(
-                        0.5, 0.5,
-                        grid[row - 1][0]['date'].strftime('%d\n%b'),
-                        horizontalalignment='center',
-                        verticalalignment='center',
-                        fontsize=14
-                    )
-                    plt.axis('off')
-                else:
-                    values = grid[row - 1][col - 1]['values']
-                    total = sum(values)
-
-                    if total > 0:
-                        axs.append(plt.axes([pie_row_header_width + (col - 1) * pie_width, (row - 1) * pie_height, pie_width, pie_height], facecolor='white'))
-                        plt.axis('off')
-                        radius = total / max_total
-                        plt.pie(values, shadow=True, explode=explode, radius=radius)
-                    else:
-                        axs.append(plt.axes([pie_row_header_width + (col - 1) * pie_width + image_padding,
-                                             (row - 1) * pie_height + image_padding,
-                                             pie_width - image_padding,
-                                             pie_height - image_padding], facecolor='white'))
-                        plt.axis('off')
-                        if grid[row - 1][col - 1]['date'] < tomorrow:
-                            plt.imshow(img, aspect=1)
-
+    for day in range(len(grid[0])):
+        draw_day_header(day)
+    for week in range(weeks):
+        draw_week_header(week)
+    for week in range(weeks):
+        for day in range(len(grid[week])):
+            values = grid[week][day]['values']
+            if sum(values) > 0:
+                draw_pie(week, day, values)
+            else:
+                draw_empty_pie(week, day)
     for ax in axs:
         ax.patch.set_visible(False)
         ax.set_xticklabels([])
@@ -121,11 +225,10 @@ if __name__ == '__main__':
     plt.style.use('grayscale')
 
     draw_plot(
-        plt.axes([plot_left, plot_bottom, plot_width, plot_height], facecolor='white'),
-        x, y,
-        ['morning', 'evening'])
+        plt.axes([plot_left, plot_bottom, plot_width, plot_height]),
+        x, y, ['morning', 'evening'])
 
-    draw_pies(grid)
+    draw_pies(grid, weeks=weeks, empty_image_file_name='old-woman.png')
 
     plt.savefig('myfig.png', dpi=dpi, cmap='gray', pad_inches=0, bbox_inches='tight')
 
