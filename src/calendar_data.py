d21ca2017b8b6c228ec8a54a56e7d1d480f5e1bb
masterAndrey <filbert@yandex.ru>
2017-04-19 08:28:44 +0300

    Image generation refactoring

--- src/calendar_data.py
+++ src/calendar_data.py
@@ -1,6 +1,43 @@
 """
 Prepare data for calendar_image.py
 """
+import copy
+import datetime
+import numpy as np
+
+
+class CalendarData(object):
+    def __init__(self, settings):
+        self.settings = settings
+        self.actions = settings['actions']
+
+    def preprocess_actions(self, button, actions):
+        """
+        Add summary (with button name value) if there is no one.
+        Substitutes {button} with button name in parameters
+        and {summary} with summary parameter.
+        """
+        actions = copy.deepcopy(actions)
+        for action in actions:
+            if 'summary' not in action:
+                action['summary'] = button
+            summary = action['summary'].format(button=button)
+            for param in action:
+                if isinstance(action[param], str):
+                    action[param] = action[param].format(button=button, summary=summary)
+        return actions
+
+    def dashboard_actions(self, dashboard):
+        """Returns list of actions for the dashboard"""
+        result = []
+        for button in self.actions:
+            if button != '__DEFAULT__':
+                actions = self.actions[button]['actions']
+                actions = self.preprocess_actions(button, actions)
+                for act in actions:
+                    result.append(act)
+        return result
+
 
 def interval_by_time(time, by_time=[]):
     if len(by_time) == 0:
@@ -74,16 +111,65 @@ def events_to_array(events, summary, by_time=[]):
     return [event['date'] for event in a], [[event['values'][0] for event in a], [event['values'][1] for event in a]]
 
 
-if __name__ == '__main__':
+def test():
+    events = [
+        {'end': datetime.datetime(2017, 4, 6, 20, 17, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 6, 20, 2, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 7, 8, 17, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 7, 8, 2, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 7, 19, 12, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 7, 18, 55, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 8, 11, 11, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 8, 11, 2, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 11, 19, 53, 19, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 11, 19, 33, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 12, 8, 30, 11, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 12, 8, 20, 50, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 12, 20, 39, 3, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 12, 20, 18, 29, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 13, 8, 19, 22, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 13, 8, 13, 24, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 13, 20, 23, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 13, 20, 3, 5, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 14, 8, 22, 30, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 14, 8, 15, 39, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 14, 17, 26, 47, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 14, 16, 55, 56, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 16, 18, 20, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 16, 17, 48, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 17, 8, 27, 43, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 17, 8, 16, 35, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'},
+        {'end': datetime.datetime(2017, 4, 17, 17, 51, 35, tzinfo=tzoffset(None, 10800)),
+         'start': datetime.datetime(2017, 4, 17, 17, 28, 31, tzinfo=tzoffset(None, 10800)),
+         'summary': '#white'}
+    ]
     x, y = events_to_array(
         events,
         'white',
-        by_time=[{'before': '11:00:00', 'summary': 'morning {button}'}, {'summary': 'evening {button}'}]
+        by_time=[{'before': '11:00:00', 'summary': 'morning {summary}'}, {'summary': 'evening {summary}'}]
     )
     print(x, y)
     grid = events_to_weeks_grid(
         events,
         'white',
-        by_time=[{'before': '11:00:00', 'summary': 'morning {button}'}, {'summary': 'evening {button}'}]
+        by_time=[{'before': '11:00:00', 'summary': 'morning {summary}'}, {'summary': 'evening {summary}'}]
     )
+    print(grid)
+
+
+if __name__ == '__main__':
+    test()
 
