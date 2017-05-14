af1832ac73b24ab8ab255b8371c42faa873a9b98
masterAndrey <filbert@yandex.ru>
2017-05-14 09:17:03 +0300

    Fixed plot data to shop gaps on plot if absent

--- src/calendar_data.py
+++ src/calendar_data.py
@@ -149,6 +149,16 @@ def events_to_array(events, absents):
             if date_str not in by_date:
                 by_date[date_str] = [0 for _ in range(len(events))]
             by_date[date_str][event_list_idx] += event_duration(event)
+    # for all absences add days with zeroes to see the gap on plot
+    for absent_list_idx, absent_list in enumerate(absents):
+        for absent in absent_list:
+            start = absent['start'].replace(hour=0, minute=0, second=0, microsecond=0)
+            end = absent['end'].replace(hour=0, minute=0, second=0, microsecond=0)
+            for day in range((end - start).days):
+                date = start + datetime.timedelta(days=day)
+                date_str = date.strftime(DATE_FMT)
+                if date_str not in by_date:
+                    by_date[date_str] = [0 for _ in range(len(events))]
     x = [datetime.datetime.strptime(date_str, DATE_FMT) for date_str in sorted(by_date.keys())]
     y = []
     for event_list_idx in range(len(events)):
@@ -161,57 +171,78 @@ def test():
     settings = load_settings()
     print(calendar_events_list(settings, 'anna_work_out'))
     events = [
-        [
-            {'end': datetime.datetime(2017, 4, 7, 8, 17, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 7, 8, 2, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Morning work-out'},
-            {'end': datetime.datetime(2017, 4, 8, 11, 11, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 8, 11, 2, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Morning work-out'},
-            {'end': datetime.datetime(2017, 4, 12, 8, 30, 11, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 12, 8, 20, 50, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Morning work-out'},
-            {'end': datetime.datetime(2017, 4, 13, 8, 19, 22, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 13, 8, 13, 24, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Morning work-out'},
-            {'end': datetime.datetime(2017, 4, 14, 8, 22, 30, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 14, 8, 15, 39, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Morning work-out'},
-            {'end': datetime.datetime(2017, 4, 17, 8, 27, 43, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 17, 8, 16, 35, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Morning work-out'},
-        ],
-        [
-            {'end': datetime.datetime(2017, 4, 6, 20, 17, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 6, 20, 2, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 7, 19, 12, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 7, 18, 55, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 11, 19, 53, 19, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 11, 19, 33, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 12, 20, 39, 3, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 12, 20, 18, 29, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 13, 20, 23, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 13, 20, 3, 5, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 14, 17, 26, 47, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 14, 16, 55, 56, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 16, 18, 20, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 16, 17, 48, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'},
-            {'end': datetime.datetime(2017, 4, 17, 17, 51, 35, tzinfo=tzoffset(None, 10800)),
-             'start': datetime.datetime(2017, 4, 17, 17, 28, 31, tzinfo=tzoffset(None, 10800)),
-             'summary': 'Physiotherapy'}
-        ]
+        [{'end': datetime.datetime(2017, 4, 14, 8, 22, 30, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 14, 8, 15, 39, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 17, 8, 27, 43, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 17, 8, 16, 35, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 18, 8, 18, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 18, 8, 12, 9, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 19, 8, 24, 26, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 19, 8, 17, 8, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 20, 8, 22, 24, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 20, 8, 16, 34, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 21, 8, 25, 27, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 21, 8, 20, 26, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 22, 10, 16, 5, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 22, 10, 10, 7, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 23, 10, 43, 37, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 23, 10, 38, 45, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 24, 8, 22, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 24, 8, 14, 47, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'},
+               {'end': datetime.datetime(2017, 4, 25, 8, 15, 7, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 25, 8, 10, 58, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Morning work-out'}
+         ],
+        [{'end': datetime.datetime(2017, 4, 13, 20, 23, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 13, 20, 3, 5, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 14, 17, 26, 47, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 14, 16, 55, 56, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 16, 18, 20, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 16, 17, 48, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 17, 17, 51, 35, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 17, 17, 28, 31, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 19, 17, 21, 57, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 19, 16, 49, 55, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 21, 17, 5, 4, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 21, 16, 26, 51, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 23, 19, 18, 49, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 23, 18, 58, 12, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 4, 25, 20, 17, 14, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 4, 25, 20, 3, 25, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 5, 10, 17, 59, 18, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 5, 10, 17, 36, 51, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'},
+               {'end': datetime.datetime(2017, 5, 12, 20, 49, 9, tzinfo=tzoffset(None, 10800)),
+                'start': datetime.datetime(2017, 5, 12, 20, 9, 32, tzinfo=tzoffset(None, 10800)),
+                'summary': 'Physiotherapy'}
+         ]
     ]
-    x, y = events_to_array(events)
-    print(x, y)
-    grid = events_to_weeks_grid(events)
-    print(grid)
+    absents = [[{'end': datetime.datetime(2017, 5, 7, 23, 59, 59, tzinfo=tzoffset(None, 10800)),
+                 'start': datetime.datetime(2017, 4, 26, 0, 0, tzinfo=tzoffset(None, 10800)),
+                 'summary': 'Sick'}],
+               []]
+    x, y = events_to_array(events, absents)
+    pprint.pprint(x)
+    pprint.pprint(y)
+    grid = events_to_weeks_grid(events, absents)
+    pprint.pprint(grid)
 
 
 if __name__ == '__main__':
