5115ef14530b80f9d774979d6f6a12f2eddd00a6
github-action-benchmark <github@users.noreply.github.com>
2024-03-22 08:51:58 +0000

    add Benchmark (pytest) benchmark result for ae347d7a999694dc42add8d4785bd5b4b45a4f06

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711097518202,
+  "repoUrl": "https://github.com/andgineer/docker-iot-calendar",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Andrei_Sorokin2@epam.com",
+            "name": "Andrei Sorokin",
+            "username": "andgineer"
+          },
+          "committer": {
+            "email": "Andrei_Sorokin2@epam.com",
+            "name": "Andrei Sorokin",
+            "username": "andgineer"
+          },
+          "distinct": true,
+          "id": "ae347d7a999694dc42add8d4785bd5b4b45a4f06",
+          "message": "new ruff formatting docstrings\nupgrade docs to material for mkdocs and disable them",
+          "timestamp": "2024-03-22T09:50:31+01:00",
+          "tree_id": "b14b13a802664a638d05cb01f7587e05c1d8df38",
+          "url": "https://github.com/andgineer/docker-iot-calendar/commit/ae347d7a999694dc42add8d4785bd5b4b45a4f06"
+        },
+        "date": 1711097517714,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_calendar_image.py::test_draw_benchmark",
+            "value": 415.5074010183354,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00009833972236785959",
+            "extra": "mean: 2.406695999997055 msec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
