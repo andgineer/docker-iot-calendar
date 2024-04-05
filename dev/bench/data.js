2bffffdff310ca22581dc4186ed7280bf57b8991
github-action-benchmark <github@users.noreply.github.com>
2024-04-05 04:27:27 +0000

    add Benchmark (pytest) benchmark result for e97bbccd709c951447b1391b0b5abee6b1964b91

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711097518202,
+  "lastUpdate": 1712291247730,
   "repoUrl": "https://github.com/andgineer/docker-iot-calendar",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00009833972236785959",
             "extra": "mean: 2.406695999997055 msec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Andrei_Sorokin2@epam.com",
+            "name": "Andrei Sorokin",
+            "username": "andgineer"
+          },
+          "committer": {
+            "email": "Andrei_Sorokin2@epam.com",
+            "name": "Andrei Sorokin",
+            "username": "andgineer"
+          },
+          "distinct": true,
+          "id": "e97bbccd709c951447b1391b0b5abee6b1964b91",
+          "message": "upgraded volnurable version of pillow\nmypy",
+          "timestamp": "2024-04-05T06:26:11+02:00",
+          "tree_id": "6c4a65d3ac4e4fb1a1ea920c9a5d6967f66b36d9",
+          "url": "https://github.com/andgineer/docker-iot-calendar/commit/e97bbccd709c951447b1391b0b5abee6b1964b91"
+        },
+        "date": 1712291247213,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_calendar_image.py::test_draw_benchmark",
+            "value": 421.04373204232473,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000934594900881652",
+            "extra": "mean: 2.3750502000098095 msec\nrounds: 5"
           }
         ]
       }
